fcc05fd8f661524834518e30f66ea39513feefe6
rvg <alexandrat@codefabric.io>
2021-04-19 16:48:37 -0400

    indentacion

--- numeros_kaprekar_normales_orden-2.py
+++ numeros_kaprekar_normales_orden-2.py
@@ -2,15 +2,15 @@ import math
 
 def divisores(n):
     resultado = []	
-    for i in range(1, int(math.sqrt(n) + 1)):		
-        if (n % i == 0):			
-            if (n / i == i):
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if(n % i == 0):
+            if(n / i == i):
                 resultado.append(i)
             else:
                 resultado.append(i)
                 resultado.append(int(n / i))
     return resultado;
-		
+
 def mcd(x, y):
     while(y):
         x, y = y, x % y
